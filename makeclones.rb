c7c1df4376b29e3c2c27b92935e0509563403cf0
Dani Roxberry <dani@bitoriented.com>
2019-12-08 16:39:44 -0800

    [add] updated release template

--- makeclones.rb
+++ makeclones.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class Makeclones < Formula
   desc ""
   homepage "https://github.com/droxey/makeclones"
-  version "snapshot"
+  version "1.0.0"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/droxey/makeclones/releases/download/vsnapshot/makeclones_darwin_amd64.zip"
-      sha256 "99ed26582eb6dc0573f646a06ea3151692e4f145b15202c0a42f5e0c04af55e9"
+      url "https://github.com/droxey/makeclones/releases/download/v1.0.0/makeclones_1.0.0_darwin_amd64.zip"
+      sha256 "61ce8cba7656c1bd20e5a14e2e5d979f26643556dc8f89168f1c671838ea4e84"
     when /linux/
-      url "https://github.com/droxey/makeclones/releases/download/vsnapshot/makeclones_linux_amd64.tar.gz"
-      sha256 "b50403786429eb2b3c8fd92d0cfec636a0856d0d7aa8ba1bc28d5eff3c961cd4"
+      url "https://github.com/droxey/makeclones/releases/download/v1.0.0/"
+      sha256 ""
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class Makeclones < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/droxey/makeclones/releases/download/vsnapshot/makeclones_darwin_386.zip"
-      sha256 "198b7f51c326110ac8586ce8b4ca90a29ab08025899dd5cba4353101064be428"
+      url "https://github.com/droxey/makeclones/releases/download/v1.0.0/makeclones_1.0.0_darwin_386.zip"
+      sha256 "d3be464c4afab53cdacf0e9ce10ca35ddd1d82a42bc355ca3dc64b3a99b23fc8"
     when /linux/
-      url "https://github.com/droxey/makeclones/releases/download/vsnapshot/makeclones_linux_386.tar.gz"
-      sha256 "5d9238d4a015b5be03a62c6471ea29dafe984d41a960c0d176f5167efdc10585"
+      url "https://github.com/droxey/makeclones/releases/download/v1.0.0/"
+      sha256 ""
     when /solaris|bsd/
       :unix
     else
